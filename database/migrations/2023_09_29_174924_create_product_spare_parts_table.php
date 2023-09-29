b8b1daa347a0425e5907ab37690910c3cf61c2b1
ryumoon88 <naufalhady08@yahoo.com>
2023-09-30 00:58:42 +0700

    Add ProductSparePart table fields

--- database/migrations/2023_09_29_174924_create_product_spare_parts_table.php
+++ database/migrations/2023_09_29_174924_create_product_spare_parts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('product_spare_parts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price');
             $table->timestamps();
         });
     }
