f219388f24a6a3f46154d85aeb5a32d385940226
ryumoon88 <naufalhady08@yahoo.com>
2023-09-30 00:41:00 +0700

    - Rename resource file to [model]Resource. - Add product table fields

--- database/migrations/2023_09_29_173226_create_products_table.php
+++ database/migrations/2023_09_29_173226_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('product_type_id');
+            $table->integer('price');
             $table->timestamps();
         });
     }
