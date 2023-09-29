567f895ab66f5028879129365469353e884724dc
ryumoon88 <naufalhady08@yahoo.com>
2023-09-30 00:50:00 +0700

    Add ProductSparePart model, resource, request, controller, database

---
+++ app/Http/Controllers/ProductSparePartController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSparePart;
+use App\Http\Requests\StoreProductSparePartRequest;
+use App\Http\Requests\UpdateProductSparePartRequest;
+
+class ProductSparePartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductSparePartRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSparePart $productSparePart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductSparePartRequest $request, ProductSparePart $productSparePart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSparePart $productSparePart)
+    {
+        //
+    }
+}
