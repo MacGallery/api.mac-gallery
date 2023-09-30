b83b8987f0889cc43bcd7650b12d47d0b8807527
ryumoon88 <naufalhady08@yahoo.com>
2023-09-30 11:31:29 +0700

    Move modification from Exception Handler to middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,16 +29,16 @@ class Handler extends ExceptionHandler
         });
     }
 
-    public function render($request, Throwable $e)
-    {
-        if ($e instanceof ModelNotFoundException && $request->getContentTypeFormat() == 'json') {
-            return response()->json([
-                'status' => [
-                    'code' => '404',
-                    'message' => 'The data you are looking for was not found'
-                ]
-            ]);
-        }
-        return parent::render($request, $e);
-    }
+    // public function render($request, Throwable $e)
+    // {
+    //     if ($e instanceof ModelNotFoundException && $request->getContentTypeFormat() == 'json') {
+    //         return response()->json([
+    //             'status' => [
+    //                 'code' => '404',
+    //                 'message' => 'The data you are looking for was not found'
+    //             ]
+    //         ]);
+    //     }
+    //     return parent::render($request, $e);
+    // }
 }
---
+++ app/Http/Middleware/SubstituteBindings.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Routing\Middleware\SubstituteBindings as BaseSubstituteBindings;
+
+class SubstituteBindings extends BaseSubstituteBindings
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $this->router->substituteBindings($route = $request->route());
+
+            $this->router->substituteImplicitBindings($route);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'The data you are looking for was not found'
+            ]);
+            // if ($route->getMissing()) {
+            //     return $route->getMissing()($request, $exception);
+            // }
+
+            // throw $exception;
+        }
+
+        return $next($request);
+    }
+}
